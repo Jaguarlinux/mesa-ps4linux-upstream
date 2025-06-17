fdfd02e3bd7a80528c521e3025500d029b8150c5
TigerClips1 <spongebob1966@proton.me>
2025-06-16 21:46:25 -0400

    Update the buildscript
    Signed-off-by: TigerClips1 <spongebob1966@proton.me>

--- buildmesa32.sh
+++ buildmesa32.sh
@@ -1,39 +1,55 @@
-#!/bin/sh
-cd build && 
-rm -rf * && 
-meson setup  \
-  --buildtype=release  \
-  --cross-file=lib32 \
-  --prefix=/usr \
-  --libdir=/usr/lib32 \
+#!/bin.sh
+mkdir -p build && \
+cd build
+meson setup \
+  --cross-file=lib32       \
+  --buildtype=release \
+  -Dprefix=/usr \
+  --libdir=/usr/lib32      \
   -Dplatforms=x11,wayland \
+  -Dvulkan-beta=true \
   -Dvulkan-drivers=amd,swrast,virtio \
-  -Dshader-cache=enabled  \
-  -Dshader-cache-max-size=8G \
   -Dvulkan-layers=device-select,overlay,screenshot \
+  -Dbuild-aco-tests=true \
+  -Dgallium-rusticl=true \
   -Dgallium-extra-hud=true \
   -Dgallium-drivers=radeonsi,r600,zink,virgl,softpipe,llvmpipe \
+  -Dgallium-nine=true \
+  -Dgallium-vdpau=enabled \
+  -Dgallium-va=enabled \
   -Dopengl=true \
+  -Dglx=auto \
   -Dgles1=enabled \
   -Dgles2=enabled \
   -Degl=enabled \
-  -Dllvm=enabled \
-  -Dlmsensors=enabled  \
-  -Dglx=dri \
+  -Dllvm=auto \
+  -Dlmsensors=enabled \
   -Dtools=glsl,nir \
-  -Dgallium-vdpau=enabled \
-  -Dgallium-va=enabled  \
-  -Dglvnd=enabled  \
-  -Dlibunwind=enabled  \
-  -Dosmesa=true \
-  -Dgallium-nine=true \
-  -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc,av1dec,av1enc,vp9dec \
+  -Dlibunwind=enabled \
+  -Dvideo-codecs=all \
   -Dlegacy-x11=dri2 \
-  -Dteflon=true \
   -Dzstd=enabled \
-  -Dshared-glapi=enabled \
+  -Damd-use-llvm=true \
+  -Dspirv-to-dxil=true \
+  -Dstatic-libclc=all \
+  -Dmesa-clc=enabled \
+  -Dinstall-mesa-clc=true \
+  -Dinstall-precomp-compiler=true \
+  -Dosmesa=true \
+  -Dprecomp-compiler=enabled \
   -Dmicrosoft-clc=disabled \
-  -Dvalgrind=disabled  \
-   .. &&
+  -Dvalgrind=disabled \
+  -Dshader-cache=enabled \
+  -Dshader-cache-max-size=8G \
+  -Dglvnd=disabled  \
+  ..&&
 sed -i 's/\/usr\/lib\//\/usr\/lib32\//g' ./build.ninja &&
 BINDGEN_EXTRA_CLANG_ARGS="-m32" ninja -j4
+
+DESTDIR=$PWD/DESTDIR ninja install                     &&
+cp -vr DESTDIR/usr/lib32/* /usr/lib32                  &&
+if [ -d DESTDIR/usr/share/vulkan ]; then
+    cp -vR DESTDIR/usr/share/vulkan /usr/share
+fi                                                     &&
+rm -rf DESTDIR                                         &&
+ldconfig
--- buildmesa64.sh
+++ buildmesa64.sh
@@ -1,37 +1,47 @@
 
-mkdir -pv build
-cd build
-meson setup  \
-  --buildtype=release \
-  -Dprefix=/usr \
-  -Dplatforms=x11,wayland \
-  -Dvulkan-drivers=amd,swrast,virtio \
-  -Dshader-cache=enabled \
-  -Dshader-cache-max-size=8G \
-  -Dvulkan-layers=device-select,overlay,screenshot \
-  -Dgallium-extra-hud=true \
-  -Dgallium-drivers=radeonsi,r600,zink,virgl,softpipe,llvmpipe \
-  -Dopengl=true \
-  -Dgles1=enabled \
-  -Dgles2=enabled \
-  -Degl=enabled \
-  -Dllvm=enabled \
-  -Dlmsensors=enabled \
-  -Dglx=dri \
-  -Dtools=glsl,nir \
-  -Dgallium-vdpau=enabled \
-  -Dgallium-va=enabled \
-  -Dglvnd=enabled \
-  -Dlibunwind=enabled \
-  -Dosmesa=true \
-  -Dgallium-nine=true \
-  -Dvideo-codecs=vc1dec,h264dec,h264enc,h265dec,h265enc,av1dec,av1enc,vp9dec \
-  -Dlegacy-x11=dri2 \
-  -Dteflon=true \
-  -Dzstd=enabled \
-  -Dshared-glapi=enabled \
-  -Dmicrosoft-clc=disabled  \
-  -Dvalgrind=disabled  \
-  .. &&
-meson configure  && \
-ninja 
+ mkdir -p build && \
+ cd build
+  meson setup \
+    --buildtype=release \
+    -Dprefix=/usr \
+    -Dplatforms=x11,wayland \
+    -Dvulkan-beta=true \
+    -Dvulkan-drivers=amd,swrast,virtio \
+    -Dvulkan-layers=device-select,overlay,screenshot \
+    -Dbuild-aco-tests=true \
+    -Dgallium-rusticl=true \
+    -Dgallium-extra-hud=true \
+    -Dgallium-drivers=radeonsi,r600,zink,virgl,softpipe,llvmpipe \
+    -Dgallium-nine=true \
+    -Dgallium-vdpau=enabled \
+    -Dgallium-va=enabled \
+    -Dopengl=true \
+    -Dglx=auto \
+    -Dgles1=enabled \
+    -Dgles2=enabled \
+    -Degl=enabled \
+    -Dllvm=auto \
+    -Dlmsensors=enabled \
+    -Dtools=glsl,nir \
+    -Dlibunwind=enabled \
+    -Dvideo-codecs=all \
+    -Dlegacy-x11=dri2 \
+    -Dzstd=enabled \
+    -Damd-use-llvm=true \
+    -Dspirv-to-dxil=true \
+    -Dstatic-libclc=all \
+    -Dmesa-clc=enabled \
+    -Dinstall-mesa-clc=true \
+    -Dinstall-precomp-compiler=true \
+    -Dosmesa=true \
+    -Dprecomp-compiler=enabled \
+    -Dmicrosoft-clc=disabled \
+    -Dvalgrind=disabled \
+    -Dshader-cache=enabled \
+    -Dshader-cache-max-size=8G \
+    -Dglvnd=disabled && \
+
+meson configure && \
+ninja -j4  && \
+ninja install && \
+cp -rv ../docs -T /usr/share/doc/mesa-25.2.0
